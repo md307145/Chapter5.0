e0bd7c19527d36632a286fbec2d6bb3d60bce44e
ManueDiaz <md307145@students.davenport.k12.ia.us>
2019-05-06 13:04:04 -0500

    Chapter5

--- src/RockPaperScissor.java
+++ src/RockPaperScissor.java
@@ -2,25 +2,63 @@ import java.util.Scanner;
 
 public class RockPaperScissor {
     public static void main(String[] args) {
-        String a;
+        int a;
+
         Scanner k=new Scanner(System.in);
+        int random = (int) (Math.random()) * 3 + 1;
 
-        System.out.println("Chose 1 for rock");
-        System.out.println("Chose 2 for paper");
-        System.out.println("Chose 3 for scissor");
-        a=k.next();
-        if(a.equals("1")){
+        display("Chose 1 for rock");
+        display("Chose 2 for paper");
+        display("Chose 3 for scissor");
+        a=k.nextInt();
+        guess(a, random);
 
-        }
-        else if (a.equals("2")){
-
-        }
-        else if(a.equals("3")){
-
-        }
-        public static void random(){}
     }
-}
+    public static void guess(int choice, int r){
+        String result = null;
+        if(choice == 1){
+            if(r==1){
+               result="Tie";
+            }
+            else if(r == 2){
+                result="Lose";
+            }
+            else if(r == 3){
+                result="Win";
+            }
+        }
+        else if (choice == 2){
+            if(r==1){
+                result="Win";
+            }
+            else if(r == 2){
+                result="Tie";
+            }
+            else if(r == 3){
+                result="Loss";
+            }
+
+        }
+        else if(choice == 3){
+            if(r==1){
+                result="Loss";
+            }
+            else if(r == 2){
+                result="Win";
+            }
+            else if(r == 3){
+                result="Tie";
+            }
+
+        }
+
+        display(result);
+    }
+    public static void display(String r){
+        System.out.println(r);
+    }
+
+        }
 
 
 
